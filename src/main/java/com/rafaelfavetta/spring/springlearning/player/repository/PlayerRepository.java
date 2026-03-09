25111816575b2231ede99bd2a0728844584e6f2e
RafaelFavetta <rafaelfavetta@gmail.com>
2026-03-09 16:38:13 -0300

    feat: update import path for PlayerModel in PlayerRepository

--- src/main/java/com/rafaelfavetta/spring/springlearning/player/repository/PlayerRepository.java
+++ src/main/java/com/rafaelfavetta/spring/springlearning/player/repository/PlayerRepository.java
@@ -1,6 +1,6 @@
 package com.rafaelfavetta.spring.springlearning.player.repository;
 
-import com.rafaelfavetta.spring.springlearning.player.controller.model.PlayerModel;
+import com.rafaelfavetta.spring.springlearning.player.model.PlayerModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
