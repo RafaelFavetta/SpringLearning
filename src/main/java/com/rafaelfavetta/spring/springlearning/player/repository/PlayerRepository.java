e2539f98cb4cd91ab0feb0083ff1eecbe07b7981
RafaelFavetta <rafaelfavetta@gmail.com>
2026-03-09 16:33:45 -0300

    feat: add PlayerRepository interface for PlayerModel

---
+++ src/main/java/com/rafaelfavetta/spring/springlearning/player/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelfavetta.spring.springlearning.player.repository;
+
+import com.rafaelfavetta.spring.springlearning.player.controller.model.PlayerModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PlayerRepository extends JpaRepository<PlayerModel, UUID> {
+}
